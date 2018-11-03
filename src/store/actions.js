a198d19d73039d45d80f6876f97cbfd42539a659
xiaoxue <794287996@qq.com>
2018-11-03 22:12:40 +0800

    1

---
+++ src/store/actions.js
@@ -0,0 +1,22 @@
+/* 通过mutation间接更新state的多个方法的对象 */
+import {
+  RECEIVE_ADDRESS,
+  RECEIVE_CATEGORYS,
+  RECEIVE_SHOPS
+} from './mutations-type'
+import {
+  reqAddress,
+  reqFoodCategorys,
+  reqShops
+} from '../api'
+export default{
+  // 异步获取地址
+  getAddress({commit,state}){
+      //发送异步请求
+      const ge
+      reqAddress(geohash)
+      //提交一个mutation
+  }
+  // 异步获取食品分类列表
+  // 异步获取商家列表
+}
---
+++ src/store/state.js
@@ -0,0 +1,7 @@
+export default{
+  latitude: 40.10038,
+  longitude: 116.36867,
+  address: {}, // 地址
+  ategorys: [], // 食品分类
+  shops: [] // 商家数组
+}
